e9b597a420c243485fedac10e7ed39148af42d1a
sas646 <sas646@cornell.edu>
2023-01-18 12:10:39 -0500

    Learned how to join

--- code/un_dataanalysis.R
+++ code/un_dataanalysis.R
@@ -50,3 +50,39 @@ gapminder_data%>%
 gapminder_data%>%
   select(country, continent, year, lifeExp)%>%
   pivot_wider(names_from = year, values_from = lifeExp)
+
+# Working with messy data
+
+co2_emissions_dirty <- read_csv("data/co2-un-data.csv", skip = 2,
+         col_names=c("region", "country", "year", "series", "value", "footnotes", "source"))
+
+co2_emissions_dirty%>%
+  select(country, year, series, value)%>%
+  mutate(series = recode(series, "Emissions (thousand metric tons of carbon dioxide)" = "total_emissions", 
+                         "Emissions per capita (metric tons of carbon dioxide)" = "per_capita_emissions"))%>%pivot_wider(names_from=series, values_from=value)
+
+# filtering rows and removing columns
+
+co2_emissions_dirty_2005 <- co2_emissions_dirty%>%filter(year == 2005)%>%
+  select(-year)
+
+co2_emissions <- co2_emissions_dirty%>%
+  select(country, year, series, value)%>%
+  mutate(series = recode(series, "Emissions (thousand metric tons of carbon dioxide)" = "total_emissions", 
+                         "Emissions per capita (metric tons of carbon dioxide)" = "per_capita_emissions"))%>%pivot_wider(names_from=series, values_from=value)%>%filter(year == 2005)%>%
+  select(-year)
+
+co2_emissions
+
+# bringing in 2007 population data
+
+gapminder_data_2007 <- read_csv("data/gapminder_data.csv")%>%filter(year == 2007)%>%
+  select(country, pop, lifeExp, gdpPercap)
+
+inner_join(gapminder_data_2007, co2_emissions, by = "country")
+
+anti_join(co2_emissions, gapminder_data_2007, by = "country")
+
+anti_join(gapminder_data_2007, co2_emissions, by = "country")
+
+full_join(co2_emissions, gapminder_data_2007)%>%View()
